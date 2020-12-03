13b4aa5f2923292c661d0b3e1e4100ac4acbe9f7
Ru <rudikraeher@gmail.com>
2020-12-02 22:00:27 -0800

    changed port connection to include environment variable

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const logger = require("morgan");
 const mongoose = require("mongoose")
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 //HTML & API ROUTES
@@ -23,6 +24,6 @@ mongoose.connect("mongodb://localhost:27017/workout", {
   .catch((err) => console.log(err));
 
 //PORT CONNECTION
-app.listen(3000, () => {
-  console.log("App running on port 3000!");
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
