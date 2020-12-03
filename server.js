98395ddc5013027bd12e5b8599cd39c6e9fcf920
Ru <rudikraeher@gmail.com>
2020-12-02 18:22:41 -0800

    updated server file routes paths

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 //HTML & API ROUTES
 app.use(require("./routes/html-routes.js"));
-app.use(require("./routes/api-route.js"));
+app.use(require("./routes/api-routes.js"));
 
 //DB CONNECTION
 mongoose.connect("mongodb://localhost:27017/workout", {
