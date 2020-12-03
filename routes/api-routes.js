051997e0a6edd48fcdc97fa767820769f3706bc1
Ru <rudikraeher@gmail.com>
2020-12-02 18:14:04 -0800

    routes folder and api routes in progress

---
+++ routes/api-routes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Workout = require("../models/workoutModel.js")
+
+router.post("/api/workouts", ({ body } ,res) => {
+    Workout.create(body)
+    .then(workoutData => {
+        res.json(workoutData)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(workoutData => {
+        res.json(workoutData);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+router.put("/api/workouts/:id", ({ _id }, res) => {
+      Workout.findOneAndUpdate({}, { $push: { excerises: _id } })
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+
+  
+
+module.exports = router;
